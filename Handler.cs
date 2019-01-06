4827c21b41dd9dcc762a5096e55420ee4f89a87e
James Dunkerley <jdunkerley@scottlogic.com>
2019-01-06 22:34:25 +0000

    Inital Handler

---
+++ Handler.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using Amazon.Lambda.Core;
+
+namespace DiabetesLog
+{
+    public static class Handler
+    {
+        public static async Task EntryPoint(ILambdaContext context) 
+        {
+            context.Logger.Log($"Started Invocation - {context.AwsRequestId }");
+            await Task.Delay(100);
+            context.Logger.Log($"Finished Invocation - {context.AwsRequestId }");
+        }
+    }
+}
